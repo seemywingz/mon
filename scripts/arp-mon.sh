fc3028fa2d1de8c2ff5effe5a57bb62de1a7cbb9
seemywings <kevin.jayne@icloud.com>
2025-03-02 17:41:58 -0500

    sys log

--- scripts/arp-mon.sh
+++ scripts/arp-mon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-iFace="wlan0" # Change to your actual interface if necessary (e.g., "wlan0")
+iFace="en0" # Change to your actual interface if necessary (e.g., "wlan0")
 
 ##
 # Check and install dependencies only if they are missing
